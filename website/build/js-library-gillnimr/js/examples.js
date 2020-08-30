0f9ee07f7795a2f33498793b5806eb997eacb794
Nimrah <nimrahg@hotmail.com>
2020-08-30 13:15:26 -0400

    remove website build folder

--- website/build/js-library-gillnimr/js/examples.js
+++
@@ -1,7 +0,0 @@
-/* Time Signature Examples */
-
-const example1 = document.querySelector(".exampleTimeSig1");
-
-const staff = new MuseStaff();
-staff.addMeasure();
-example1.appendChild(staff.staff)
